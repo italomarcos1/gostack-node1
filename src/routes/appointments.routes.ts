a4bac971e588d8d0b94000c01c333329f0c06e63
Italo M <italomarcos0010@gmail.com>
2020-05-05 21:24:50 -0300

    done

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+import { isEqual, startOfHour, parseISO } from 'date-fns';
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find((appointment) =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'Já há um agendamento marcado pra esse horário.' });
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
