65e3e6f666f94a15a4688084bc1fa8f9d5112faa
Italo M <italomarcos0010@gmail.com>
2020-05-05 22:32:22 -0300

    Model de agendamento

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
   return response.json({ message: 'Hello World!' });
