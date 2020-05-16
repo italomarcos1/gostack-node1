2916a7afd3eb221ae50ac062771191f81b12ef3d
Italo M <italomarcos0010@gmail.com>
2020-05-16 12:55:40 -0300

    gerando token jwt

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) throw new Error('Incorrect email/password combination');
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) throw new Error('Incorrect email/password combination');
+
+    const token = sign({}, '92f87a8be2396dde0d50cdfba3bd91b5', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
