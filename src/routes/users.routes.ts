0be425b0a87453b71206c7ac46f1ebdaac82e941
Italo M <italomarcos0010@gmail.com>
2020-05-17 14:58:37 -0300

    rundown done

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(multerConfig);
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
+
+export default usersRouter;
