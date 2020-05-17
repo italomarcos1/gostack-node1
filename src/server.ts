6735607ed7006eed06ba18255fd4953b9d753aa8
Italo M <italomarcos0010@gmail.com>
2020-05-17 16:01:29 -0300

    servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
+import uploadConfig from './config/multer';
 import routes from './routes';
 
 import './database';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.get('/', (request, response) => {
