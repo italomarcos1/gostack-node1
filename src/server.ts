70bd7cf0660b336c4bfd2fe9e817d9c6651b6d27
Italo M <italomarcos0010@gmail.com>
2020-05-29 21:31:51 -0300

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import express, { response } from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import './database';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
