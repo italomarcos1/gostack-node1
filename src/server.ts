9b157c6cd20c83eb2a4ddca7e1482a9decbc4644
Italo M <italomarcos0010@gmail.com>
2020-06-27 19:24:46 -0300

    fix master

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
 });
 
