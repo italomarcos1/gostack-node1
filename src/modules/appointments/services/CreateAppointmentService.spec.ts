e8ccc330e2a465c4a96eb94a9bd4453bed780481
Italo M <italomarcos0010@gmail.com>
2020-08-24 16:04:41 -0300

    debugando

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,19 @@
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      provider_id: '7012',
+      date: new Date(),
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('7012');
+  });
+});
